9780d15f7338d1c8d0f12974c67589ad0ff60209
IrsyadHalimi <irsyadhalimi08@gmail.com>
2024-11-18 09:44:08 +0700

    menambahkan upload logo

--- app.js
+++ app.js
@@ -1,31 +1,61 @@
 function bracketApp() {
   return {
     teams: [], // List of team names
+    logos: [], // List of team logos
     bracket: [], // Generated bracket
+    isGenerated: false, // Flag to track if bracket has been generated
 
     addTeam() {
       this.teams.push(''); // Add empty input for team
+      this.logos.push(''); // Add empty slot for logo
+    },
+
+    handleLogoUpload(index, event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.logos[index] = e.target.result; // Save image as Base64
+        };
+        reader.readAsDataURL(file);
+      }
+    },
+
+    shuffleTeams(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
     },
 
     generateBracket() {
-      // Check if number of teams is valid
       const validTeams = this.teams.filter(team => team.trim() !== '');
       const teamCount = validTeams.length;
-      if (teamCount < 2 || (teamCount & (teamCount - 1)) !== 0) {
-        alert('Number of teams must be a power of 2 (e.g., 4, 8, 16).');
+
+      if (teamCount < 2) {
+        alert('Number of teams must be at least 2 (two)!');
         return;
       }
 
-      // Generate initial matches
+      const shuffledTeams = this.shuffleTeams(
+        validTeams.map((team, index) => ({ team, logo: this.logos[index] }))
+      );
+      
+
       this.bracket = [];
-      for (let i = 0; i < validTeams.length; i += 2) {
-        this.bracket.push([validTeams[i], validTeams[i + 1]]);
+      for (let i = 0; i < shuffledTeams.length; i += 2) {
+        this.bracket.push([shuffledTeams[i], shuffledTeams[i + 1]]);
       }
+
+      this.isGenerated = true;
     },
 
     resetBracket() {
       this.teams = [];
+      this.logos = [];
       this.bracket = [];
+      this.isGenerated = false;
     }
   };
 }
